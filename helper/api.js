3f933ba170ed1bf4c42db0665af7f43a297bd88e
Ezhook <srctickter@gmail.com>
2025-02-15 12:25:42 +0530

    sorted the api

--- helper/api.js
+++ helper/api.js
@@ -1,11 +1,14 @@
 import axios from "axios";
 
 const API_URL = "https://restcountries.com/v3.1/all";
-// Helper function to fetch nationality data
+
 export const fetchNationalities = async () => {
   try {
     const response = await axios.get(API_URL);
-    return response.data;
+    const sortedData = response.data.sort((a, b) =>
+      a.name.common.localeCompare(b.name.common)
+    );
+    return sortedData;
   } catch (error) {
     console.error("Error fetching nationalities:", error);
     return [];
