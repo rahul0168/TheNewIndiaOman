3602d0b80192d8eee5a4db72ff68344d0d307219
rahul0168 <rahul.naikmule.facebook@gmail.com>
2025-02-17 13:24:41 +0530

    new profile screenn added and  bugs fixed

--- app/ProfileViewScreen.js
+++ app/ProfileViewScreen.js
@@ -18,15 +18,15 @@ const ProfileViewScreen = () => {
       <View style={styles.cardContainer}>
         {/* Profile Section */}
 
-        <View style={styles.profileContainer}>
+        <View style={styles.profileContainer} className="mb-4">
           <TouchableOpacity>
-            <Text style={styles.addNameButton}>Add Your Name</Text>
+            <Text style={styles.addNameButton}>John  </Text>
           </TouchableOpacity>
           <Text style={styles.phoneNumber}>+968 123455</Text>
         </View>
 
-        {/* ABHA Card Section */}
-        <TouchableOpacity style={styles.abhaContainer}>
+       
+        {/* <TouchableOpacity style={styles.abhaContainer}>
           <View>
             <Text style={styles.abhaTitle}>Create your ABHA Number</Text>
             <Text style={styles.abhaSubtitle}>
@@ -37,7 +37,7 @@ const ProfileViewScreen = () => {
             source={require("../assets/images/abhaImage.webp")}
             style={styles.abhaImage}
           />
-        </TouchableOpacity>
+        </TouchableOpacity> */}
 
         {/* Options Section */}
         <View style={styles.optionsContainer}>
